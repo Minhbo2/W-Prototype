c561251cf03f31ebab788fb9910b5cb99a6f03d7
MINH BO <minhbo0000@gmail.com>
2018-08-06 12:16:19 -0800

    Init Project

---
+++ WistlingPrototype/Assets/Script/InputController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+///<summary>
+/// ADD SCRIPT SUMMARY
+///</summary>
+
+public class InputController : MonoBehaviour {
+
+
+    //Private Variables Members
+    [SerializeField]
+    private LayerMask CubeMask;
+
+
+	//Public Variables Members
+
+
+	//Accessors
+
+
+	//=================================================================================
+	//UNITY'S METHODS
+
+	void Start () {
+		
+	}
+	
+
+   
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit Hit;
+            Ray RayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(RayOrigin, out Hit, float.PositiveInfinity, CubeMask))
+            {
+                if (Hit.transform.gameObject != null)
+                    Destroy(Hit.transform.gameObject);
+            }
+        }
+	}
+
+
+
+	//=================================================================================
+	//Private Functions
+
+
+
+
+	//=================================================================================
+	//Public Functions
+}
